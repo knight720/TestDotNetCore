771f3959ccc00931cc8e2da33a38121483f375c3
Knight <knight.huang@bora-corp.com>
2017-05-19 13:45:19 +0800

    [test]excel and mysql

---
+++ Console/002_AccessMySQL/002_AccessMySQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _002_AccessMySQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Console/003_AccessExcel/003_AccessExcel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _003_AccessExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
