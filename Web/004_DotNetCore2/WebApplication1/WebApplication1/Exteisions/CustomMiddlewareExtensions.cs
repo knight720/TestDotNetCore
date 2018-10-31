06dc0dcc3f9d432f90404ac6ab6f28c009216e55
Knight Huang <knighthuang@91app.com>
2018-10-31 08:51:28 +0800

    004 - Middleware 擴充方法

---
+++ Web/004_DotNetCore2/WebApplication1/WebApplication1/Exteisions/CustomMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace WebApplication1.Exteisions
+{
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseFirstMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<FirstMiddleware>();
+        }
+    }
+}
--- Web/004_DotNetCore2/WebApplication1/WebApplication1/Startup.cs
+++ Web/004_DotNetCore2/WebApplication1/WebApplication1/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Exteisions;
 
 namespace WebApplication1
 {
@@ -29,7 +30,9 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime appLifetime)
         {
-            app.UseMiddleware<FirstMiddleware>();
+            //app.UseMiddleware<FirstMiddleware
+            //改用擴充方法
+            app.UseFirstMiddleware();
 
             app.Map("/second", mapApp =>
             {
