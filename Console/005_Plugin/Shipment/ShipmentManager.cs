88395925ba1d7d9aab75aee84b2d9f1b7b013789
Knight Huang <knighthuang@91app.com>
2019-03-07 09:47:03 +0800

    005 load assembly

---
+++ Console/005_Plugin/Shipment/ShipmentManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+namespace Shipment
+{
+    public class ShipmentManager
+    {
+        IShipment _shipment;
+
+        public ShipmentManager()
+        {
+            var assembly = Assembly.LoadFile(@"D:\Code\Test\TestDotNetCore\Console\005_Plugin\FirstShipment\bin\Debug\netstandard2.0\FirstShipment.dll");
+            var modules = assembly.GetLoadedModules();
+            modules.ToList().ForEach(i => Console.WriteLine(i.Name));
+            //this._shipment = (IShipment)assembly.CreateInstance("FirstShipment.FirstShipment");
+        }
+
+        public object GetCourier()
+        {
+            return this._shipment.GetCourier();
+        }
+    }
+}
