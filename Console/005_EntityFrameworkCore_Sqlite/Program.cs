c2978c5d30c5576777bc9322c372546fa1a4154e
Knight Huang <ninku720@gmail.com>
2019-03-09 22:44:18 +0800

    005 - Sqlite

---
+++ Console/005_EntityFrameworkCore_Sqlite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _005_EntityFrameworkCore_Sqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
