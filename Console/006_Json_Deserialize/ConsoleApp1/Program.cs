7fb9e1d7baa19985802124067be460a5050d088b
Knight Huang <knighthuang@91app.com>
2019-03-11 19:03:25 +0800

    006 Deserialize Dictionary

---
+++ Console/006_Json_Deserialize/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ConsoleApp1.ZipCode;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var zipCode = new ZipCodeService();
+            var data = zipCode.GetZipCode("MY");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Console/006_Json_Deserialize/ConsoleApp1/ZipCode/ZipCodeService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1.ZipCode
+{
+    public class ZipCodeService
+    {
+        public object GetZipCode(string country)
+        {
+            var json = File.ReadAllText($@".\ZipCode\{country}.json");
+            return JsonConvert.DeserializeObject<IEnumerable<IDictionary<string, object>>>(json);
+            //return JsonConvert.DeserializeObject<IEnumerable<Dictionary<string, IEnumerable<Dictionary<string, IEnumerable<Dictionary<string,object>>>>>>(json);
+
+        }
+    }
+}
