3f33f21e06bfd6d1fe2c90ac1b15f194b6ec4d22
Knight Huang <knighthuang@91app.com>
2019-04-01 18:48:37 +0800

    008 - 排除 ipv6

--- Console/008_WhatIsMyIP/Knight.WhatIsMyIP/Program.cs
+++ Console/008_WhatIsMyIP/Knight.WhatIsMyIP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Text;
 
 namespace Knight.WhatIsMyIP
 {
@@ -13,14 +14,20 @@ namespace Knight.WhatIsMyIP
             // 取得本機的 IpHostEntry 類別實體
             IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
 
+            StringBuilder sb = new StringBuilder();
+
             // 取得所有 IP 位址
             int num = 1;
             foreach (IPAddress ipaddress in iphostentry.AddressList)
             {
-                Console.WriteLine("IP #" + num + ": " + ipaddress.ToString());
+                var strIP = ipaddress.ToString();
+                //// 排除IPv6
+                if (strIP.Contains(":")) continue;
+                sb.AppendLine("IP #" + num + ": " + strIP);
                 num = num + 1;
             }
 
+            Console.WriteLine(sb.ToString());
             Console.ReadLine();
         }
     }
