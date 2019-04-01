bd9b5f0e4093e8f548df3b2dfb2e2593d88f0c32
Knight Huang <knighthuang@91app.com>
2019-04-01 09:31:00 +0800

    008 - Query Host IP

---
+++ Console/008_WhatIsMyIP/Knight.WhatIsMyIP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace Knight.WhatIsMyIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 取得本機名稱
+            String strHostName = Dns.GetHostName();
+
+            // 取得本機的 IpHostEntry 類別實體
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // 取得所有 IP 位址
+            int num = 1;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + num + ": " + ipaddress.ToString());
+                num = num + 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
