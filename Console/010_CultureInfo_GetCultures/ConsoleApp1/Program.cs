9bbeb7d59774ad60a732e26446d9ca61145661c6
Knight Huang <knighthuang@91app.com>
2019-05-07 17:38:35 +0800

    Console - 010 - CultureInfo.GetCultures(CultureTypes.AllCultures)

---
+++ Console/010_CultureInfo_GetCultures/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+            Console.ReadLine();
+        }
+    }
+}
