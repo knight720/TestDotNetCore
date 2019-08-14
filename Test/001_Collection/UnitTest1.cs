a39622681d0314458289147f05267a3847a47bd0
Knight Huang <knighthuang@91app.com>
2019-08-14 14:42:57 +0800

    T001 - first test

--- Test/001_Collection/UnitTest1.cs
+++ Test/001_Collection/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
 using Xunit;
 
 namespace _001_Collection
@@ -8,8 +11,17 @@ namespace _001_Collection
         [Fact]
         public void Test1()
         {
+            IEnumerable collection = new[] { 1, 2, 5, 8 };
 
+            collection.Should().NotBeEmpty()
+                .And.HaveCount(4)
+                .And.ContainInOrder(new[] { 2, 5 })
+                .And.ContainItemsAssignableTo<int>();
 
+            collection.Should().Equal(new List<int> { 1, 2, 5, 8 });
+            collection.Should().Equal(1, 2, 5, 8);
+            collection.Should().BeEquivalentTo(8, 2, 1, 5);
+            collection.Should().NotBeEquivalentTo(new[] { 8, 2, 3, 5 });
         }
     }
 }
