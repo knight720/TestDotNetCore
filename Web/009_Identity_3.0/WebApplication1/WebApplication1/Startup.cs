d02013bb70ae35fab7ba73b0c824d244825432e7
Knight Huang <knighthuang@91app.com>
2019-11-13 09:11:20 +0800

    191113 -  設定 Identity

--- Web/009_Identity_3.0/WebApplication1/WebApplication1/Startup.cs
+++ Web/009_Identity_3.0/WebApplication1/WebApplication1/Startup.cs
@@ -24,6 +24,7 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,10 +46,12 @@ namespace WebApplication1
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
