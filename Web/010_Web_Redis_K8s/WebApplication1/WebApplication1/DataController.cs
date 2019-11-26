e2844949f6fbaf8f03dafe49349bf148d9519615
Knight Huang <knighthuang@91app.com>
2019-11-26 23:53:21 +0800

    Web - 010 - Create WebApi

---
+++ Web/010_Web_Redis_K8s/WebApplication1/WebApplication1/DataController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            return "knight";
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostData([FromQuery][FromBody]string name)
+        {
+            return this.Ok(name);
+        }
+    }
+}
--- Web/010_Web_Redis_K8s/WebApplication1/WebApplication1/Startup.cs
+++ Web/010_Web_Redis_K8s/WebApplication1/WebApplication1/Startup.cs
@@ -24,6 +24,7 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +51,7 @@ namespace WebApplication1
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
