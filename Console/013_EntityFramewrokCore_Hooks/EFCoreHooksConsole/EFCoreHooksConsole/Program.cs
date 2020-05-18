fcbed61bb3b54586d8e9f1a0e153bb4dcb429073
Knight Huang <knighthuang@91app.com>
2020-05-18 09:06:02 +0800

    Console - 013 - Create Solution

---
+++ Console/013_EntityFramewrokCore_Hooks/EFCoreHooksConsole/EFCoreHooksConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreHooksConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
