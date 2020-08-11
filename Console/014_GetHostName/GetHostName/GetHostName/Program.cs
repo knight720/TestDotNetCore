01a1a18572c8edcaa51238f3d45e15f2bb092804
Knight Huang <knighthuang@91app.com>
2020-08-11 08:43:40 +0800

    200811 - Console Get Host Name

---
+++ Console/014_GetHostName/GetHostName/GetHostName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace GetHostName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Dns.GetHostName());
+        }
+    }
+}
