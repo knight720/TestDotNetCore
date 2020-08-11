c1a367234d3f89d9483c4d711d51e0a66ade6e68
Knight Huang <knighthuang@91app.com>
2020-08-11 08:50:03 +0800

    200811 - Create Project

---
+++ Console/015_DockerCompose/RedisConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedisConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
