b0f0f30a9ee77283b5845f86c266653cd8883515
Knight Huang <knighthuang@91app.com>
2020-08-11 10:02:46 +0800

    200811 - Access Redis

--- Console/015_DockerCompose/RedisConsole/Program.cs
+++ Console/015_DockerCompose/RedisConsole/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using StackExchange.Redis;
 
 namespace RedisConsole
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            AccessRedis();
+
+            Console.WriteLine("Finish!!!");
+        }
+
+        private static void AccessRedis()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis");
+            IDatabase db = redis.GetDatabase();
+            db.StringSet("KeyDateTime", DateTime.Now.ToLongTimeString());
+
+            var value = db.StringGetAsync("KeyDateTime").Result;
+
+            Console.WriteLine($"Value: {value}");
         }
     }
 }
