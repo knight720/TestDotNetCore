f0cf8f3e35edb66e4803311a5aa9b31edc7f997f
Knight Huang <knighthuang@91app.com>
2020-10-26 09:20:31 +0800

    Web - 013 - add WebApi

---
+++ Web/013_TagSystem/TagSystem/Controllers/Apis/DataController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TagSystem.Controllers.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("OK");
+        }
+    }
+}
--- Web/013_TagSystem/TagSystem/Startup.cs
+++ Web/013_TagSystem/TagSystem/Startup.cs
@@ -46,6 +46,8 @@ namespace TagSystem
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
