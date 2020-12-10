3563a933056c908caafe2392272ea98697e4e8d6
Knight Huang <knighthuang@91app.com>
2020-12-10 09:33:48 +0800

    Web - 201210 - test CreateDefaultBuilder

---
+++ Web/013_TagSystem/TagSystem/Controllers/Apis/InfoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace TagSystem.Controllers.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public InfoController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(this._configuration["Debug"]);
+
+            return Ok(stringBuilder.ToString());
+        }
+    }
+}
