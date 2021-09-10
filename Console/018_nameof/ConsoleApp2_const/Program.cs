4659b6c1f52c6a542de0742b16e21fa9f00ee92e
Knight Huang <knighthuang@91app.com>
2021-09-10 13:35:28 +0800

    Console - 210910 - const 編譯

---
+++ Console/018_nameof/ConsoleApp2_const/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2_const
+{
+    internal class Program
+    {
+        private int var_normal = 1;
+        private readonly int var_readonly = 2;
+        private const int var_const = 3;
+
+        private static void Main(string[] args)
+        {
+            new Program().Print();
+        }
+
+        private void Print()
+        {
+            Console.WriteLine(var_normal);
+            Console.WriteLine(var_readonly);
+            Console.WriteLine(var_const);
+        }
+    }
+}
