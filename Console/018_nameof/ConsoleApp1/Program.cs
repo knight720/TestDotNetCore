68e81930e172b9f71a6b909936dd1a872e7ef2e2
Knight Huang <knighthuang@91app.com>
2021-09-10 13:25:45 +0800

    Console - 210910 - nameof 效能

---
+++ Console/018_nameof/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Season.Summer.ToString());
+            Console.WriteLine(nameof(Season.Summer));
+        }
+    }
+
+    internal enum Season
+    {
+        Spring,
+        Summer,
+        Fall,
+        Winter
+    }
+}
