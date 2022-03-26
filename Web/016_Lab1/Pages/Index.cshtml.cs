230df6501896b86055e78484a7ae7e5163434c7e
Knight Huang <knighthuang@nine-yi.com>
2022-03-26 22:56:39 +0800

    Web - 220326 - configuration

--- Web/016_Lab1/Pages/Index.cshtml.cs
+++ Web/016_Lab1/Pages/Index.cshtml.cs
@@ -1,20 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebAPI.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
         _logger = logger;
+        this._configuration = configuration;
     }
 
     public void OnGet()
     {
         this._logger.LogDebug("Debug");
         this._logger.LogInformation("Information");
+        this._logger.LogInformation($"Domain: {this._configuration["GOV_API:DomainName"]}");
     }
 }
