921728ef97cb1035f36dad745469471eff7f835b
Knight Huang <knighthuang@nine-yi.com>
2022-06-19 21:41:15 +0800

    Web - 220619 - add Health checks

--- Web/017_Routing/WebApplication1/Program.cs
+++ Web/017_Routing/WebApplication1/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapHealthChecks("/healthz");
 
 app.Run();
