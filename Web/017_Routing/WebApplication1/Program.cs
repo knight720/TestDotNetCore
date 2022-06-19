1025c06807ea3f138d5e71c05e96e58cfa09d8f2
Knight Huang <knighthuang@nine-yi.com>
2022-06-19 22:54:06 +0800

    Web - 220619 - 實作 health check

--- Web/017_Routing/WebApplication1/Program.cs
+++ Web/017_Routing/WebApplication1/Program.cs
@@ -1,5 +1,8 @@
+using WebApplication1.Services;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecks()
+    .AddCheck<MyHealthCheck>("My");
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ Web/017_Routing/WebApplication1/Services/MyHealthCheck.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace WebApplication1.Services
+{
+    public class MyHealthCheck : IHealthCheck
+    {
+        private readonly Random _random = new Random();
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return this._random.Next(2) < 1 ? Task.FromResult(HealthCheckResult.Healthy()) : Task.FromResult(HealthCheckResult.Degraded());
+        }
+    }
+}
