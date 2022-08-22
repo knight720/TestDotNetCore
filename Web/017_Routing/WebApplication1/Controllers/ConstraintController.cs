9bc7519cd8d4e9ec08452d4751ccc827809796ec
Knight Huang <knighthuang@nine-yi.com>
2022-08-22 23:37:57 +0800

    Web - 220822 - add constraint

---
+++ Web/017_Routing/WebApplication1/Controllers/ConstraintController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    public class ConstraintController : Controller
+    {
+        [HttpGet("{value:int}")]
+        public IActionResult Index1(int value)
+        {
+            return Ok($"Constraint Index1 {value}");
+        }
+
+        [HttpGet("{value:alpha}")]
+        public IActionResult Index2(string value)
+        {
+            return Ok($"Constraint Index2 {value}");
+        }
+    }
+}
--- Web/017_Routing/WebApplication1/Program.cs
+++ Web/017_Routing/WebApplication1/Program.cs
@@ -4,20 +4,98 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecks()
     .AddCheck<MyHealthCheck>("My");
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/Home", () => "Home");
-app.MapPost("/Post", () => "Post");
-app.MapPost("/PostValue/{value}", (string value) => $"Post: {value}");
-app.MapHealthChecks("/healthz");
+#region Private Method
 
-app.MapControllers();
+void Test()
+{
+}
 
-//// Overwrite All Rule
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync("Only Hello world!");
-//});
+// 使用 WebApplication 建立中介軟體管線
+void First_Enpoint()
+{
+    //// Overwrite All Rule
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("EndPoint: Hello world!");
+    });
+}
+
+// 路由的基本概念
+void Routing_Basics()
+{
+    app.Use(async (context, next) =>
+    {
+        Console.WriteLine($"custom middleware 1, endpoint: {context.GetEndpoint()?.DisplayName ?? "null"}");
+
+        await next(context);
+    });
+
+    app.UseRouting();
+
+    app.Use(async (context, next) =>
+    {
+        Console.WriteLine($"custom middleware 2, endpoint: {context.GetEndpoint()?.DisplayName ?? "null"}");
+
+        await next(context);
+    });
+
+    app.MapGet("/", () => "Hello World!");
+}
+
+// ASP.NET Core端點定義
+void Endpoint_Definition()
+{
+    // Location 1: before routing runs, endpoint is always null here.
+    app.Use(async (context, next) =>
+    {
+        Console.WriteLine($"1. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+        await next(context);
+    });
+
+    app.UseRouting();
+
+    // Location 2: after routing runs, endpoint will be non-null if routing found a match.
+    app.Use(async (context, next) =>
+    {
+        Console.WriteLine($"2. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+        await next(context);
+    });
+
+    // Location 3: runs when this endpoint matches
+    app.MapGet("/", (HttpContext context) =>
+    {
+        Console.WriteLine($"3. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+        return "Hello World!";
+    }).WithDisplayName("Hello");
+
+    app.UseEndpoints(_ => { });
+
+    // Location 4: runs after UseEndpoints - will only run if there was no match.
+    app.Use(async (context, next) =>
+    {
+        Console.WriteLine($"4. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+        await next(context);
+    });
+}
+
+void Routing_Controller()
+{
+    app.MapControllers();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+}
+
+#endregion Private Method
+
+// Execute
+//First_Enpoint();
+//Routing_Basics();
+//Endpoint_Definition();
+Routing_Controller();
 
 app.Run();
