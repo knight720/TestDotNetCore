43ab4780485f1babc17e48688b48541067da3c10
Knight Huang <knighthuang@nine-yi.com>
2022-12-14 08:34:14 +0800

    Console - 221214 - Json Serialize Format

---
+++ Console/019_JsonSerialize/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var a = new AClass();
+Console.WriteLine("WriteIndented = defulat");
+Console.WriteLine(JsonSerializer.Serialize(a));
+Console.WriteLine("WriteIndented = true");
+Console.WriteLine(JsonSerializer.Serialize(a, new JsonSerializerOptions { WriteIndented = true }));
+
+Console.ReadLine();
+
+internal class AClass
+{
+    public int MyProperty { get; set; }
+    public int Second { get; set; }
+}
