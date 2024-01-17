9e09307e0963c6dbeb75434531188f9a85ac4374
Knight Huang <knighthuang@nine-yi.com>
2024-01-17 19:55:06 +0800

    Web - 240117 - 建立專案

---
+++ Web/020_DI_Lifetime/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var a = File.ReadAllText(@"D:\Temp\221223_PromotionService\OTM\230803\OTM_TW_QA.csv");
+
+Console.WriteLine(a.Length);
