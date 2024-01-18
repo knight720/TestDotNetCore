235f24b88c1312be6dbc36b3e8289acbe05db281
Knight Huang <knighthuang@nine-yi.com>
2024-01-18 13:44:39 +0800

    Web - 240118 - 引用套件並設定

--- Web/021_HttpLoggingMiddleware/WebApplication1/Controllers/WeatherForecastController.cs
+++ Web/021_HttpLoggingMiddleware/WebApplication1/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace WebApplication1.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
--- Web/021_HttpLoggingMiddleware/WebApplication1/Program.cs
+++ Web/021_HttpLoggingMiddleware/WebApplication1/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestHeaders.Add("sec-ch-ua");
+    logging.ResponseHeaders.Add("MyResponseHeader");
+    logging.MediaTypeOptions.AddText("application/javascript");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+    logging.CombineLogs = true;
+});
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseHttpLogging();
 
 app.UseHttpsRedirection();
 
