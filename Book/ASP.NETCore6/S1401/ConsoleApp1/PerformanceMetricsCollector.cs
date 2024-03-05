d63668882faed8693e336cb630791874248d34ae
Knight Huang <knighthuang@nine-yi.com>
2024-03-05 08:41:07 +0800

    Book - 240305 - sample it work

---
+++ Book/ASP.NETCore6/S1401/ConsoleApp1/PerformanceMetricsCollector.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp1
+{
+    public sealed class PerformanceMetricsCollector : IHostedService
+    {
+        private IDisposable? _scheduler;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _scheduler = new Timer(Callback, null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5));
+            return Task.CompletedTask;
+
+            static void Callback(object? state) => Console.WriteLine($"[{DateTimeOffset.Now}]{PerformanceMetrics.Create()}");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _scheduler?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
--- Book/ASP.NETCore6/S1401/ConsoleApp1/Program.cs
+++ Book/ASP.NETCore6/S1401/ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+new HostBuilder()
+    .ConfigureServices(svcs => svcs
+    .AddSingleton<IHostedService, PerformanceMetricsCollector>())
+    .Build()
+    .Run();
