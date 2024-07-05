84f79b3142f92270e9a2eb4f91ada21bec05e025
knighthuang720 <knighthuang@nine-yi.com>
2024-07-05 09:17:22 +0800

    Web - 240705 - print reques info

--- Web/022_ReceiveRedirectInfo/WebApplication1/WebApplication1/Program.cs
+++ Web/022_ReceiveRedirectInfo/WebApplication1/WebApplication1/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Approach 1: Terminal Middleware.
+app.Use(async (context, next) =>
+{
+    if (true)
+    {
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogInformation($"{context.Request.Method} {context.Request.Scheme}://{context.Request.Host}{context.Request.Path}");
+        using (var stream = new StreamReader(context.Request.Body))
+        {
+            var requestBody = await stream.ReadToEndAsync();
+            logger.LogInformation($"{requestBody}");
+        }
+
+        await context.Response.WriteAsync("Hello World");
+
+        return;
+    }
+
+    await next(context);
+});
 
 app.Run();
