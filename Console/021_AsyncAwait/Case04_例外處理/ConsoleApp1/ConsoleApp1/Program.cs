a27041e4da345c42c95d52975d3b61fa0c9720e1
Knight Huang <knighthuang@nine-yi.com>
2024-10-15 08:44:51 +0800

    Console - 241015 - Case04 例外處理

---
+++ Console/021_AsyncAwait/Case04_例外處理/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    DoAsync();
+    //await DoAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Catch: {ex.Message}");
+}
+
+Console.WriteLine("2");
+
+async Task DoAsync()
+{
+    await Task.Delay(1000);
+    throw new Exception("Some Exception");
+}
