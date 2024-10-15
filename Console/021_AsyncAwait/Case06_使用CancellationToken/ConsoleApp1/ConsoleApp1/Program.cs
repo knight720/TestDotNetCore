ff9f759281c03e1d89d4f65d6169e32e2154b37f
Knight Huang <knighthuang@nine-yi.com>
2024-10-15 09:51:35 +0800

    Console - 241015 - 使用 CancellationToken

---
+++ Console/021_AsyncAwait/Case06_使用CancellationToken/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿CancellationTokenSource source = new CancellationTokenSource();
+CancellationToken token = source.Token;
+
+var task = DoAsync(token);
+
+await Task.Delay(1000);
+
+Console.WriteLine("2");
+
+//source.Cancel();
+task.Wait();
+
+async Task DoAsync(CancellationToken cancellationToken)
+{
+    try
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"DoAsync: {i}");
+            await Task.Delay(300);
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+    }
+    catch (OperationCanceledException ex)
+    {
+        Console.WriteLine($"Exception: {ex.Message}");
+    }
+
+    Console.WriteLine("1");
+}
