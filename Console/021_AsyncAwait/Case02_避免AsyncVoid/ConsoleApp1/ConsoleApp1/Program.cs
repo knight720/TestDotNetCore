e925390f27b01b84d0183daac97c60419a9d71c0
Knight Huang <knighthuang@nine-yi.com>
2024-10-18 08:30:39 +0800

    Console - 241018 - 避免 async void

--- Console/021_AsyncAwait/Case02_避免AsyncVoid/ConsoleApp1/ConsoleApp1/Program.cs
+++ Console/021_AsyncAwait/Case02_避免AsyncVoid/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,18 @@
-﻿DoAsync();
-//await DoAsync()
+﻿try
+{
+    DoAsync();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"EX: {ex.Message}");
+}
 
+Thread.Sleep(1000);
 Console.WriteLine("2");
 
 async void DoAsync()
 {
-    await Task.Delay(1000);
+    await Task.Delay(100);
     Console.WriteLine("1");
+    throw new ApplicationException("Some Error!");
 }
