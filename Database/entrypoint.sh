274c32ddc037eef68060ead48d40470587fd989e
knight720 <ninku720@yahoo.com.tw>
2025-06-12 08:25:48 +0800

    修正資料庫還原腳本，更新為 AdventureWorksLT2022 資料庫及備份檔案名稱

--- Database/entrypoint.sh
+++ Database/entrypoint.sh
@@ -8,11 +8,11 @@ echo "Waiting for SQL Server to start..."
 sleep 20
 
 # 還原 AdventureWorks2022 資料庫
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P P@ssw0rd -Q \
-"RESTORE DATABASE AdventureWorks2022
-FROM DISK = N'/var/opt/mssql/backup/AdventureWorks2022.bak'
-WITH MOVE 'AdventureWorks2022' TO '/var/opt/mssql/data/AdventureWorks2022.mdf',
-     MOVE 'AdventureWorks2022_log' TO '/var/opt/mssql/data/AdventureWorks2022_log.ldf',
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P P@ssw0rd -C -Q \
+"RESTORE DATABASE AdventureWorksLT2022
+FROM DISK = N'/var/opt/mssql/backup/AdventureWorksLT2022.bak'
+WITH MOVE 'AdventureWorksLT2022_Data' TO '/var/opt/mssql/data/AdventureWorksLT2022.mdf',
+     MOVE 'AdventureWorksLT2022_log' TO '/var/opt/mssql/data/AdventureWorksLT2022_log.ldf',
      REPLACE"
 
 # 前景啟動 SQL Server
